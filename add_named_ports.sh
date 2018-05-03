f67800a04294aceb9b6fe1b005ab6510839084c1
Dan Isla <disla@google.com>
2018-05-03 09:51:01 -0700

    switch to using GOOGLE_APPLICATION_CREDENTIALS instead of activate-service-account

--- add_named_ports.sh
+++ add_named_ports.sh
@@ -6,9 +6,16 @@ JQ=$(command -v jq || true)
 
 eval "$(${JQ} -r '@sh "INSTANCE_GROUP=\(.instance_group) NAME=\(.name) PORT=\(.port)"')"
 
+TMP_FILE=$(mktemp)
+function cleanup() {
+  rm -f "${TMP_FILE}"
+}
+trap cleanup EXIT
+
 if [[ ! -z ${GOOGLE_CREDENTIALS+x} && ! -z ${GOOGLE_PROJECT+x} ]]; then
-    gcloud auth activate-service-account --key-file - <<<"${GOOGLE_CREDENTIALS}"
-    gcloud config set project "${GOOGLE_PROJECT}"
+  echo "${GOOGLE_CREDENTIALS}" > ${TMP_FILE}
+  export GOOGLE_APPLICATION_CREDENTIALS=${TMP_FILE}
+  gcloud config set project "${GOOGLE_PROJECT}"
 fi
 
 RES=$(gcloud compute instance-groups set-named-ports ${INSTANCE_GROUP} --named-ports=${NAME}:${PORT} --format=json)
